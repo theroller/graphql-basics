9b67a740905d36d73e05d32d18b370acb9fc4f7d
James Roller, Jr <james.roller@aspencapital.com>
2019-12-06 15:06:52 -0800

    chore: install esm and startup graphqlserver

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log('hi');
+import { GraphQLServer } from 'graphql-yoga';
+
+// Type Definitions (schema)
+const typeDefs = `
+    type Query {
+        hello: String!
+    }
+`;
+
+// Resolvers
+const resolvers = {
+    Query: {
+        hello() {
+            return 'This is my first query!';
+        }
+    }
+};
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers,
+});
+
+server.start(() => {
+    console.log('server is up');
+});
