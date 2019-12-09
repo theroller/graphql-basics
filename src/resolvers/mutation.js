176acb7bb228bf4d47c96cc6dfc110a95cb825ae
James Roller, Jr <james.roller@aspencapital.com>
2019-12-08 19:43:54 -0800

    feat: add updateUser

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -77,6 +77,30 @@ const Mutation = {
 
         return deletedUser;
     },
+    updateUser(parent, { id, data }, { db }) {
+        const user = db.users.find(user => user.id == id);
+        if (!user) {
+            throw new Error(`user id ${id} not found`);
+        }
+
+        if (typeof data.email === 'string') {
+            const emailTaken = db.users.some(user => user.email === data.email);
+            if (emailTaken) {
+                throw new Error(`email ${data.email} taken`);
+            }
+            user.email = data.email;
+        }
+
+        if (typeof data.name === 'string') {
+            user.name = data.name;
+        }
+
+        if (typeof data.age !== 'undefined') {
+            user.age = data.age;
+        }
+
+        return user;
+    },
 };
 
 export { Mutation as default };
