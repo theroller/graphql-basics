d56e06b802f93f3bb16a4fffdc314a36049c9d23
James Roller, Jr <james.roller@aspencapital.com>
2019-12-08 20:17:34 -0800

    feat: add dummy subscription to a counter

--- src/index.js
+++ src/index.js
@@ -1,26 +1,29 @@
-import { GraphQLServer } from 'graphql-yoga';
+import { GraphQLServer, PubSub } from 'graphql-yoga';
 
 import db from './db';
 import Query from './resolvers/query';
 import Mutation from './resolvers/mutation';
+import Subscription from './resolvers/subscription';
 import Comment from './resolvers/comment';
 import Post from './resolvers/post';
 import User from './resolvers/user';
 
-const resolvers = {
-    Query,
-    Mutation,
-    Comment,
-    Post,
-    User,
-};
+const pubsub = new PubSub();
 
 const server = new GraphQLServer({
     context: {
         db,
+        pubsub,
     },
     typeDefs: './src/schema.graphql',
-    resolvers,
+    resolvers: {
+        Query,
+        Mutation,
+        Subscription,
+        Comment,
+        Post,
+        User,
+    },
 });
 
 server.start(() => {
